1ab7131a38eeae948a55f89c21f67e40abc0154a
SuzanFah <susiefahim@gmail.com>
2024-08-28 07:32:09 +0300

    add videos section and try it

--- panduta_landing_page/app.py
+++ panduta_landing_page/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 
 # Initialize Flask application
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 # Define route for the landing page
 @app.route('/')
 def index():
     # Render the HTML template 'index.html'
     return render_template('index.html')
+@app.route('/test-video')
+def test_video():
+     return send_from_directory('static/videos', 'panda_laundry.mp4')
 
 # Run the Flask application
 if __name__ == '__main__':
